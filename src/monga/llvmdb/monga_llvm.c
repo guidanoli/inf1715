59f07474ebe91b1ed94e6c1aa6eefb10a340e5b1
Guilherme Dantas <guidanoli@hotmail.com>
2020-11-10 21:11:30 -0300

    llvm skeleton

---
+++ src/monga/llvmdb/monga_llvm.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#include "monga.y.h"
+#include "monga_ast.h"
+#include "monga_ast_bind.h"
+#include "monga_ast_print.h"
+#include "monga_ast_destroy.h"
+
+void yyerror(const char* err)
+{
+    fprintf(stderr, "%s (line %zu)\n", err, monga_get_lineno());
+}
+
+int main(int argc, char** argv)
+{
+    int res = yyparse();
+    if (!res) {
+        monga_ast_program_bind(root);
+        /*monga_ast_program_llvm(root);*/
+        monga_ast_program_destroy(root);
+
+        if (monga_get_allocated_cnt() != 0) {
+            fprintf(stderr, "Memory leak detected.\n");
+#ifdef MONGA_DEBUG
+            monga_clean_amb();
+#endif
+            return MONGA_ERR_LEAK;
+        }
+    }
+    return res;
+}
